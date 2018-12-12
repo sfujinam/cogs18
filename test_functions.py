0832715ddb015389367bde0bb2630aa9a8ada16a
sfujinam <44536586+sfujinam@users.noreply.github.com>
2018-12-12 03:39:42 -0800

    Add files via upload

---
+++ test_functions.py
@@ -0,0 +1,16 @@
+"""Test for my functions.
+
+Note: because these are 'empty' functions (return None), here we just test
+  that the functions execute, and return None, as expected.
+"""
+
+from my_module.functions import main_menu
+
+##
+##
+
+def test_1():
+
+    assert callable(main_menu)
+    
+    
